c964e12d4b0421acced8b66f3584a1718fb2ac55
lo1cgsan <lo1cgsan@gmail.com>
2018-03-29 14:19:32 +0200

    Dodanie aplikacji do projektu, pierwsza migracja aplikacji, autoreprezentacja modeli

--- ankiety/ankieta/models.py
+++ ankiety/ankieta/models.py
@@ -1,10 +1,23 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Pytanie(models.Model):
     pytanie_tekst = models.CharField(max_length=200)
     pub_data = models.DateTimeField('data publikacji')
 
+    def __str__(self):
+        """reprezentacja obiektu"""
+        return self.pytanie_tekst
+
+    def opublikowane_ostatnio(self):
+        return self.pub_data >= timezone.now() - datetime.timedelta(days=1)
+
 class Odpowiedz(models.Model):
     pytanie = models.ForeignKey(Pytanie, on_delete=models.CASCADE)
     wybor_tekst = models.CharField(max_length=200)
     glosy = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.wybor_tekst
