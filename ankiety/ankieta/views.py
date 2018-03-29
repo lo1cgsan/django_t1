2b474bd6a760dba65ba90ffa098facfee4f7b5e0
lo1cgsan <lo1cgsan@gmail.com>
2018-03-29 18:21:38 +0200

    widok pobierający dane z bazy bez szablonu

--- ankiety/ankieta/views.py
+++ ankiety/ankieta/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Pytanie
+
+
 def index(request):
-    return HttpResponse("Witaj świecie!")
+    ostatnie_pytania_lista = Pytanie.objects.order_by('-pub_data')[:5]
+    wyjscie = ', '.join([p.pytanie_tekst for p in ostatnie_pytania_lista])
+    return HttpResponse(wyjscie)
 
 def szczegoly(request, pytanie_id):
     return HttpResponse("Przeglądasz pytanie %s." % pytanie_id)
