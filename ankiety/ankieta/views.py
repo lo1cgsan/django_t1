cf369a125f0ba3899b4580a3b76e527eb0cb654a
lo1cgsan <lo1cgsan@gmail.com>
2018-03-29 18:59:50 +0200

    użycie skrótu render()

--- ankiety/ankieta/views.py
+++ ankiety/ankieta/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+# from django.template import loader
 
 from .models import Pytanie
 
 
 def index(request):
     ostatnie_pytania_lista = Pytanie.objects.order_by('-pub_data')[:5]
-    szablon = loader.get_template('ankieta/index.html')
-    kontekst = {
-        'ostatnie_pytania_lista': ostatnie_pytania_lista,
-    }
-    return HttpResponse(szablon.render(kontekst, request))
+    kontekst = {'ostatnie_pytania_lista': ostatnie_pytania_lista}
+    return render(request, 'ankieta/index.html', kontekst)
 
 def szczegoly(request, pytanie_id):
     return HttpResponse("Przeglądasz pytanie %s." % pytanie_id)
