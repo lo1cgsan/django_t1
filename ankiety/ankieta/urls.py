bcc6ec0e3712ee628ad557826ec1809498ac1a04
lo1cgsan <lo1cgsan@gmail.com>
2018-03-29 17:48:30 +0200

    podstawowe widoki z parametrem oraz konfiguracja URL-i

--- ankiety/ankieta/urls.py
+++ ankiety/ankieta/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:pytanie_id>/', views.szczegoly, name='szczegoly'),
+    path('<int:pytanie_id>/wyniki', views.wyniki, name='wyniki'),
+    path('<int:pytanie_id>/glos', views.glos, name='glos'),
 ]
--- ankiety/ankieta/views.py
+++ ankiety/ankieta/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Witaj świecie!")
+
+def szczegoly(request, pytanie_id):
+    return HttpResponse("Przeglądasz pytanie %s." % pytanie_id)
+
+def wyniki(request, pytanie_id):
+    response = "Przeglądasz wyniki pytania %s."
+    return HttpResponse(response % pytanie_id)
+
+def glos(request, pytanie_id):
+    return HttpResponse("Głosujesz na pytanie %s." % pytanie_id)
