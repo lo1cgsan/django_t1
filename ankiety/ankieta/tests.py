3f5a44480defcf5c51973d30205ac7b3371e1700
lo1cgsan <lo1cgsan@gmail.com>
2018-03-30 12:45:06 +0200

    dodanie testu wykrywającego błąd

--- ankiety/ankieta/tests.py
+++ ankiety/ankieta/tests.py
@@ -1,3 +1,18 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Pytanie
+
+
+class PytanieModelTests(TestCase):
+
+    def test_opublikowany_ostatnio_z_pytaniem_w_przyszlosci(self):
+        """
+        opublikowane_ostatnio() zwraca False dla pytań z pub_data
+        w przyszłości.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        przyszle_pytanie = Pytanie(pub_data=time)
+        self.assertIs(przyszle_pytanie.opublikowane_ostatnio(), False)
