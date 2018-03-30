115dd803e6b89d4ec9157fea925ec284f3c3aca4
lo1cgsan <lo1cgsan@gmail.com>
2018-03-30 12:55:40 +0200

    dodanie 2 nowych testów

--- ankiety/ankieta/tests.py
+++ ankiety/ankieta/tests.py
@@ -8,11 +8,29 @@ from .models import Pytanie
 
 class PytanieModelTests(TestCase):
 
-    def test_opublikowany_ostatnio_z_pytaniem_w_przyszlosci(self):
+    def test_opublikowane_ostatnio_z_pytaniem_w_przyszlosci(self):
         """
-        opublikowane_ostatnio() zwraca False dla pytań z pub_data
+        opublikowane_ostatnio() zwracać ma False dla pytań z pub_data
         w przyszłości.
         """
         time = timezone.now() + datetime.timedelta(days=30)
         przyszle_pytanie = Pytanie(pub_data=time)
         self.assertIs(przyszle_pytanie.opublikowane_ostatnio(), False)
+
+    def test_opublikowane_ostatnio_z_stare_pytanie(self):
+        """
+        opublikowane_ostatnio() zwracać ma False dla pytań których pub_data
+        jest starsza niż 1 dzień.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        stare_pytanie = Pytanie(pub_data=time)
+        self.assertIs(stare_pytanie.opublikowane_ostatnio(), False)
+
+    def test_opublikowane_ostatnio_with_ostatnie_pytanie(self):
+        """
+        opublikowane_ostatnio() zwracać ma True dla pytań których pub_data
+        jest w ciągu ostatniego dnia.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        ostatnie_pytanie = Pytanie(pub_data=time)
+        self.assertIs(ostatnie_pytanie.opublikowane_ostatnio(), True)
