3b87803ea0dcbed6a226a80e60ed925930be614c
lo1cgsan <lo1cgsan@gmail.com>
2018-03-31 14:58:29 +0200

    dodanie modelu Odpowiedz do strony administracyjnej

--- ankiety/ankieta/admin.py
+++ ankiety/ankieta/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Pytanie
+from .models import Pytanie, Odpowiedz
 
 admin.site.register(Pytanie)
+admin.site.register(Odpowiedz)
