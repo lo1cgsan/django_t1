2b56ca3f8b62fdfcf6e63c6c669f54aaa8314a0f
lo1cgsan <lo1cgsan@gmail.com>
2018-04-01 13:52:38 +0200

    przykład użycia fieldset w panelu admina

--- ankiety/ankieta/admin.py
+++ ankiety/ankieta/admin.py
@@ -6,5 +6,12 @@ from .models import Pytanie, Odpowiedz
 class PytanieAdmin(admin.ModelAdmin):
     fields = ['pub_data', 'pytanie_tekst']
 
+class OdpowiedzAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Pytanie i odpowiedź', {'fields': ['pytanie']}),
+        (None, {'fields': ['wybor_tekst']}),
+        ('Oddane głosy', {'fields': ['glosy']}),
+    ]
+
 admin.site.register(Pytanie, PytanieAdmin)
-admin.site.register(Odpowiedz)
+admin.site.register(Odpowiedz, OdpowiedzAdmin)
